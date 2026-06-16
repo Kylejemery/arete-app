e63e3662d39bb031ef22d7a585bb356b6a4f2a30
Kyle <kemery9585@gmail.com>
2026-06-16 16:38:10 -0400

    version 1.3.1

--- academy/web/.next/types/validator.ts
+++ academy/web/.next/types/validator.ts
@@ -245,6 +245,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/linkedin-callback/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/linkedin-callback">> = Specific
+  const handler = {} as typeof import("../../src/app/api/linkedin-callback/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/api/schedule-posts/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/schedule-posts">> = Specific
